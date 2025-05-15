843e9aedfa0cea8a6607391fe92f41bd79137cbb
tuanadr <tuanadr@gmail.com>
2025-05-15 15:04:20 +0700

    Sửa menu

--- video-downloader-saas/frontend/src/components/ui/Navbar.js
+++ video-downloader-saas/frontend/src/components/ui/Navbar.js
@@ -52,7 +52,7 @@ const Navbar = () => {
                   </svg>
                 </button>
                 {isQuickToolsOpen && (
-                  <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10 top-full"> {/* Added top-full to position below button */}
+                  <div className="origin-top-right absolute right-0 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10 top-full"> {/* Removed mt-2, Added top-full to position below button */}
                     <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
                       <Link to="/tai-video-youtube" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">
                         Tải Video YouTube
